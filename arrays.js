abf57f2b1ee604db918ca086ae16de2bf201e7fc
Emma Fewer <emmafewer20@gmail.com>
2020-09-15 00:31:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,7 @@ function destructivelyAddElementToBeginningOfArray(a,b) {
   return [1,2,3,4]
   destructivelyAddElementToBeginningOfArray.unshift('n')
 }
+
+function destructivelyAddElementToBeginningOfArray(a,b) {
+  return
+}
