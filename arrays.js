7ec24333c4de22e4c94c77c3ae734ec2bdf324db
Emma Fewer <emmafewer20@gmail.com>
2020-09-15 00:30:38 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = 
+
+function addElementToBeginningOfArray(a,b) {
+ return [1,2,3,4]
+ addElementToBeginningOfArray.unshift('n')
+}
+
+function destructivelyAddElementToBeginningOfArray(a,b) {
+  return [1,2,3,4]
+  destructivelyAddElementToBeginningOfArray.unshift('n')
+}
